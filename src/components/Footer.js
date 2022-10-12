c540be6905898348a849def9cbfcb3226218fcc2
ryksyy <miko.prykari@gmail.com>
2022-10-12 12:42:02 +0300

    finished task

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer(){
+    return(
+        <footer classname='footer'>
+            <div className='container'>
+                <span>Please stick a footer here</span>
+            </div>
+        </footer>
+    );
+}
